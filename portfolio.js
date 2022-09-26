9360cf792c855370f622edd4b7fc57d78e297215
jeff <>
2022-09-26 15:27:13 -0400

    updated css, background image, website content

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,18 @@
 const hamburg = document.querySelector(".hamburger2");
 const sideBar = document.querySelector(".active");
 const sideBarClose = document.querySelector(".sidebar_close");
+const navLink = document.querySelector(".nav_link");
+const navItem = document.querySelector(".nav_item");
+
+navLink.addEventListener("click", () => {
+    sideBar.classList.toggle("active");
+    hamburg.style.visibility = 'visible';
+})
+
+navItem.addEventListener("click", () => {
+    sideBar.classList.toggle("active");
+    hamburg.style.visibility = 'visible';
+})
 
 hamburg.addEventListener("click", () =>{
     sideBar.classList.toggle("active");
